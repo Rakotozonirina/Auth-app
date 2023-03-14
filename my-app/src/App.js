8736d352bfe6800a841a6eacb0005c51e6f6631e
Zo Nirina <tomzonirina@gmail.com>
2023-03-14 11:00:40 +0300

    I changed the framework to Ant Design

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import Navbar from './Compenents/Navbar';
 
 function App() {
   return (
     <>
-
+      <Navbar/>
     </>
   );
 }
---
+++ my-app/src/Compenents/Navbar.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { Divider } from 'antd';
+
+const Navbar = () => {
+return (
+    <header>
+        <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed nonne merninisti licere mihi ista
+            probare, quae sunt a te dicta? Refert tamen, quo modo.
+        </p>
+        <Divider orientation='left' plain>Text</Divider>
+        <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed nonne merninisti licere mihi ista
+            probare, quae sunt a te dicta? Refert tamen, quo modo.
+        </p>
+        <Divider/>
+        <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed nonne merninisti licere mihi ista
+            probare, quae sunt a te dicta? Refert tamen, quo modo.
+        </p>
+        <Divider/>
+        <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed nonne merninisti licere mihi ista
+            probare, quae sunt a te dicta? Refert tamen, quo modo.
+        </p>
+    </header>
+)
+}
+
+export default Navbar;
