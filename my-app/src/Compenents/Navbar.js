3e04adc69a783a047a0b755e6a10245f96cce5a4
Zo Nirina <tomzonirina@gmail.com>
2023-03-14 11:47:53 +0300

    I start with the navbar

--- my-app/src/Compenents/Navbar.js
+++ my-app/src/Compenents/Navbar.js
@@ -1,28 +1,22 @@
 import React from 'react';
-import { Divider } from 'antd';
+import { Row, Col } from 'antd';
+import Picture from '../Pictures/phosphor-logo-bold-1.svg'
+const style = {
+    background: '#2D2727',
+    padding: '1.5rem 3rem',
+    color: '#F0EB8D',
+}
 
 const Navbar = () => {
 return (
     <header>
-        <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed nonne merninisti licere mihi ista
-            probare, quae sunt a te dicta? Refert tamen, quo modo.
-        </p>
-        <Divider orientation='left' plain>Text</Divider>
-        <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed nonne merninisti licere mihi ista
-            probare, quae sunt a te dicta? Refert tamen, quo modo.
-        </p>
-        <Divider/>
-        <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed nonne merninisti licere mihi ista
-            probare, quae sunt a te dicta? Refert tamen, quo modo.
-        </p>
-        <Divider/>
-        <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed nonne merninisti licere mihi ista
-            probare, quae sunt a te dicta? Refert tamen, quo modo.
-        </p>
+        <Row>
+            <Col span={12} style={style} className='gutter-row'>
+                <img src={Picture} alt='The Logo'/>
+            </Col>
+            <Col span={6} style={style} className='gutter-row'>Name</Col>
+            <Col span={6} style={style} className='gutter-row'>Name</Col>
+        </Row>
     </header>
 )
 }
