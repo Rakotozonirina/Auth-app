bf7709057d9499e21e2779da68794c9ef782f0cf
Zo Nirina <tomzonirina@gmail.com>
2023-03-15 11:07:29 +0300

    I adding the hero component

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Navbar from './Compenents/Navbar';
+import Hero from './Compenents/Hero';
 
 function App() {
   return (
     <>
       <Navbar/>
+      <Hero/>
     </>
   );
 }
---
+++ my-app/src/Compenents/Hero.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Hero.css';
+
+
+const Hero = () => {
+return (
+    <main>
+        <div className='hero-image'>
+            <div className='hero-content'>
+
+            </div>
+        </div>
+    </main>
+)
+}
+
+export default Hero;
