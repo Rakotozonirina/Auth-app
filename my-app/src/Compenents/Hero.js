7e9fe9929f3a72d16e00a60d489e4e5185de727d
Zo Nirina <tomzonirina@gmail.com>
2023-03-15 11:44:49 +0300

    I finished the hero page

--- my-app/src/Compenents/Hero.js
+++ my-app/src/Compenents/Hero.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import './Hero.css';
-
+import { SyncOutlined } from '@ant-design/icons';
+import { Space, Button } from 'antd';
+import { Typography } from 'antd';
+const {Title} = Typography;
 
 const Hero = () => {
 return (
     <main>
         <div className='hero-image'>
             <div className='hero-content'>
-
+                <Title level={3} style={{color:'#F0EB8D'}}>Welcome to Expense Tracker!</Title>
+                <Title level={5} style={{color:'#F0EB8D'}}>Add, view, edit, and delete expenses</Title>
+                <Space wrap> <Button type='text' style={{color:'#F0EB8D'}} >LOGIN/REGISTER<SyncOutlined spin style={{fontSize:'1rem'}} /></Button></Space>
             </div>
         </div>
     </main>
