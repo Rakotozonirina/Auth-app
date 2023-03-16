deeacac9a5cf09967e5f0f4b929946683e341cc8
Zo Nirina <tomzonirina@gmail.com>
2023-03-16 23:19:01 +0300

    The auth is okey

--- my-app/src/Compenents/Dashboard.js
+++ my-app/src/Compenents/Dashboard.js
@@ -13,18 +13,25 @@ const style = {
     color: '#F0EB8D',
 }
 const Dashboard = () => {
-    const otherAuth = getAuth();
-    const otherUser = otherAuth.currentUser;
-    
-    
     const [user, loading, error] = useAuthState(auth);
     const navigate = useNavigate();
+    const otherAuth = getAuth();
+    const otherUser = otherAuth.currentUser;
     useEffect(() => {
         if(loading){
             return;
         }
-        if(!user) return navigate("/");
-    }, [user, loading]);
+        if (!user) {
+            return navigate("/");
+        }
+        if (otherUser !== null && otherUser !== undefined) {
+            // do something with otherUser
+            console.log("One user is loggin");
+        } else {
+            // handle the case where otherUser is null or undefined
+            console.log("null variable");
+        }
+    }, [user, loading, navigate]);
 return (
     <div>
         <Row>
@@ -33,7 +40,7 @@ return (
                     <img src={Picture} style={{marginBlockStart:'0.6em'}} alt='The Logo'/>
                 </Space>
             </Col>
-            <Col xs={7} sm={6} md={8} lg={9} xl={7} style={style} className='gutter-row'> <Space wrap><Button type='text' style={{color: '#F0EB8D'}}>{otherUser.email}</Button></Space></Col>
+            <Col xs={7} sm={6} md={8} lg={9} xl={7} style={style} className='gutter-row'> <Space wrap>{otherUser && <Button type='text' style={{color: '#F0EB8D'}}>{otherUser.email}</Button>}</Space></Col>
             <Col xs={8} sm={6} md={4} lg={3} xl={3} style={style} className='gutter-row'>
                 <Space wrap>
                     <Button type='text' style={{color: '#F0EB8D'}} onClick={logout} >Log Out</Button>
