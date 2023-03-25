b526a731cdfdf435dce3c360363e19726dc905a0
Zo Nirina <tomzonirina@gmail.com>
2023-03-25 23:22:42 +0300

    I have a bug to show the date of the commpenent

--- my-app/src/Compenents/FileView.js
+++ my-app/src/Compenents/FileView.js
@@ -1,14 +1,14 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { storage } from '../firebase';
-import { ref, listAll } from 'firebase/storage';
+import { ref, list } from 'firebase/storage';
 import { BarsOutlined } from '@ant-design/icons';
 import { Button } from 'antd';
 const FileView = () => {
     const [files, setFiles] = useState([]);
     const fecthFiles = async () => {
         const storageRef = ref(storage, `documents-pdf`);
-        listAll(storageRef)
-        .then((res) => {
+        const firstPage = list(storageRef, {maxResults: 5});
+        firstPage.then((res) => {
             res.items.forEach((itemRef) => {
             // All the items under listRef.
             setFiles(arr => [...arr, itemRef.name]);
@@ -17,12 +17,27 @@ const FileView = () => {
             // Uh-oh, an error occurred!
             alert(error);
         });
+        if(firstPage.nextPageToken){
+            const secondPage = list(storageRef, {
+                maxResults: 5,
+                pageToken: firstPage.nextPageToken,
+            });
+            console.log(secondPage.items);
+            console.log(secondPage.prefixes);
+        }
+    }
+    const StorageDate = () => {
+        const storageDat = ref(storage);
+        const postDate = storageDat.name;
+        return(
+            <p>{postDate}</p>
+        )
     }
 return (
     <div>
         <Button type='text' style={{color: '#F0EB8D', display: 'flex', justifyContent:'center'}} onClick={fecthFiles}>Regarder les listes<BarsOutlined style={{marginBlockStart: '0.4em'}}/></Button>
-        {files.map((val) => (
-            <p style={{color: '#F0EB8D'}}>{val}</p>
+        {files.map((file) => (
+                <p key={file.id} style={{color: '#F0EB8D'}}>{file}<StorageDate/></p>
         ))}
     </div>
 )
