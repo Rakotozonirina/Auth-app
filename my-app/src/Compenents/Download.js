1bb1b0f3ad98ba2b6c77ff91b64f9e3c478e867a
Zo Nirina <tomzonirina@gmail.com>
2023-03-27 22:34:46 +0300

    I need to resolve this problem

--- my-app/src/Compenents/Download.js
+++ my-app/src/Compenents/Download.js
@@ -1,9 +1,9 @@
 import React, { useState } from 'react';
 import { useNavigate } from "react-router-dom";
 import { storage } from '../firebase';
-import { ref, list, getDownloadURL } from 'firebase/storage';
+import { ref, list, getDownloadURL, getStorage } from 'firebase/storage';
 import { Button } from 'antd';
-
+import { saveAs } from 'file-saver';
 const Download = () => {
     const navigate = useNavigate();
     const HandleBack = () => {
@@ -29,9 +29,11 @@ const Download = () => {
             console.log(secondPage.prefixes);
         };
     };
-    const downloadFiles = () => {
-        const storageRefference = ref(storage, 'gs://expen-e8cdc.appspot.com/documents-pdf');
-        getDownloadURL(ref(storageRefference))
+    /*const downloadFiles = () => {
+        //const storageRefference = ref(storage, 'gs://expen-e8cdc.appspot.com/documents-pdf');
+        //getDownloadURL(ref(storageRefference))
+        const storage = getStorage();
+        getDownloadURL(ref(storage, 'gs://expen-e8cdc.appspot.com/documents-pdf'))
         .then((url) => {
             const xhr = new XMLHttpRequest();
             xhr.responseType = 'blob';
@@ -46,14 +48,30 @@ const Download = () => {
             // Handle any errors
             alert(error);
         });
-    }
+    }*/
+    const downloadFiles = (file) => {
+    const storageRef = ref(storage, file);
+    getDownloadURL(storageRef)
+    .then((url) => {
+        fetch(url)
+        .then(res => res.blob())
+        .then(blob => {
+          const fileName = file.split('/').pop(); // Get the file name from the full path
+          saveAs(blob, fileName); // Download the file using `saveAs` from `file-saver`
+        })
+    })
+    .catch((error) => {
+      // Handle any errors
+        alert(error);
+    });
+};
 return (
     <div>
         <h2>Listes des fichiers PDF</h2>
         <Button type='text' onClick={HandleBack}>Retour</Button>
         <Button type='text' onClick={fecthFiles}>Listes</Button>
                 {files.map((file) => (
-                    <p key={file.id} onClick={downloadFiles}>
+                    <p key={file.id} onClick={() => downloadFiles(file)}>
                         {file}
                     </p>
                 ))}
