0bb1ce27dc276a13ac19f72a29379fa85c24e9ba
Zo Nirina <tomzonirina@gmail.com>
2023-03-27 15:24:23 +0300

    I have got a error in the download file

--- my-app/src/Compenents/Download.js
+++ my-app/src/Compenents/Download.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { useNavigate } from "react-router-dom";
 import { storage } from '../firebase';
-import { ref, list } from 'firebase/storage';
+import { ref, list, getDownloadURL } from 'firebase/storage';
 import { Button } from 'antd';
 
 const Download = () => {
@@ -15,7 +15,7 @@ const Download = () => {
         const firstPage = list(storageRef, { maxResults: 5 } );
         firstPage.then((res) => {
             res.items.forEach((itemRef) => {
-                setFiles(arr => [...arr, itemRef.name]);
+                setFiles(arr => [...arr, itemRef.fullPath]);
             });
         }).catch((error) => {
             alert(error);
@@ -28,6 +28,24 @@ const Download = () => {
             console.log(secondPage.items);
             console.log(secondPage.prefixes);
         };
+    };
+    const downloadFiles = () => {
+        const storageRefference = ref(storage, 'gs://expen-e8cdc.appspot.com/documents-pdf');
+        getDownloadURL(ref(storageRefference))
+        .then((url) => {
+            const xhr = new XMLHttpRequest();
+            xhr.responseType = 'blob';
+            xhr.onload = (event) => {
+                const blob = xhr.response;
+            };
+            xhr.open('GET', url);
+            xhr.send();
+            console.log(url);
+        })
+        .catch((error) => {
+            // Handle any errors
+            alert(error);
+        });
     }
 return (
     <div>
@@ -35,7 +53,7 @@ return (
         <Button type='text' onClick={HandleBack}>Retour</Button>
         <Button type='text' onClick={fecthFiles}>Listes</Button>
                 {files.map((file) => (
-                    <p key={file.id}>
+                    <p key={file.id} onClick={downloadFiles}>
                         {file}
                     </p>
                 ))}
