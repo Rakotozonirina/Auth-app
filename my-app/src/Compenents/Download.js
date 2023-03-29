0e2113a6449faaacec18afc4d1097ee814aa0a5f
Zo Nirina <tomzonirina@gmail.com>
2023-03-29 15:01:32 +0300

    I finish to install the icons in the Download

--- my-app/src/Compenents/Download.js
+++ my-app/src/Compenents/Download.js
@@ -3,8 +3,12 @@ import { useNavigate } from "react-router-dom";
 import { storage } from '../firebase';
 import { ref, list, getDownloadURL } from 'firebase/storage';
 import { Button } from 'antd';
+import { VerticalAlignBottomOutlined } from '@ant-design/icons';
 import { saveAs } from 'file-saver';
+import { Scrollbars } from 'react-custom-scrollbars';
 import './Download.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.min.js';
 import Picture from '../Pictures/phosphor-logo-bold-1.svg';
 const Download = () => {
     const navigate = useNavigate();
@@ -17,7 +21,7 @@ const Download = () => {
         const firstPage = list(storageRef, { maxResults: 10 } );
         firstPage.then((res) => {
             res.items.forEach((itemRef) => {
-                setFiles(arr => [...arr, itemRef.fullPath]);
+                setFiles(arr => [...arr, itemRef.name]);
             });
         }).catch((error) => {
             alert(error);
@@ -48,28 +52,36 @@ const Download = () => {
     });
 };
 return (
-    <div className='main-content'>
-        <section>
-        <img src={Picture} alt='Logo'/>
-        </section>
-        <section>
-        <section>
-        <Button type='text' onClick={HandleBack}>Retour</Button>
-        <Button type='text' onClick={fecthFiles}>Listes</Button>
-        </section>
-        <section>
-        <h2>Listes des fichiers PDF</h2>
-        </section>
-        </section>
-        
-        <section>
-        {files.map((file) => (
-                    <p key={file.id} onClick={() => downloadFiles(file)}>
-                        {file}
-                    </p>
-        ))}
-        </section>
+    <Scrollbars style={{ height: '100vh' }}>
+        <div className='main-content'>
+            <section className='content-image-navbar'>
+                <section className='image-content'>
+                    <div className='logo-content'>
+                        <img src={Picture} alt='Logo'/>
+                    </div>
+                </section>
+                <section className='content-pdf'>
+                    <section className='navbar-content'>
+                        <Button type='text' onClick={fecthFiles}>Listes</Button>
+                        <Button type='text' onClick={HandleBack}>Retour</Button>
+                    </section>
+                    <section className='title-file-content bg-light'>
+                        <section className='py-3 px-5 fs-1 fw-bolder'>
+                            <h2>Listes des fichiers PDF</h2>
+                        </section>
+                        <section>
+                            {files.map((file) => (
+                                        <p key={file.id} className='fw-lighter'>
+                                            {file}
+                                            <VerticalAlignBottomOutlined onClick={() => downloadFiles(file)} style={{marginBlockStart: '0.4em'}}/>
+                                        </p>
+                            ))}
+                        </section>
+                    </section>
+                    </section>
+            </section>
     </div>
+    </Scrollbars>
 )
 }
 
